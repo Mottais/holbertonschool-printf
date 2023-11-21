991225c5c8b42a69fcdf11297f25c7aff64c3799
Mottais <mottais.afy@outlook.fr>
2023-11-21 00:16:05 -0800

    _printf %c %s %%

--- test/main.c
+++ test/main.c
@@ -14,23 +14,30 @@ int main() {
 	*/  
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
-	printf("%d %d\n", len, len2);
+	printf("Length:[%d, %i]\n", len, len);
+    	printf("Length:[%d, %i]\n", len2, len2);
 
 	len =_printf("Character:[%c]\n", 'H');
 	len2 =printf("Character:[%c]\n", 'H');
-	printf("%d %d\n", len, len2);
+	printf("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);
 
 	len =_printf("String:[%s]\n", "I am a string !");
 	len2 =printf("String:[%s]\n", "I am a string !");
-	printf("%d %d\n", len, len2);
+	printf("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);
 
 	len = _printf("Percent:[%%]\n");
 	len2 = printf("Percent:[%%]\n");
-	printf("%d %d\n", len, len2);
+	printf("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);	
+
 
 	len = _printf("Unknown:[%r]\n");
 	len2 = printf("Unknown:[%r]\n");
-	printf("%d %d\n", len, len2);
+	printf("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);	
+
 	return (0);
 }
 
