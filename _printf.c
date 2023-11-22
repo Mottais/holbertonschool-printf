5c3772116e471c1ee5562bee9949717f55c7a6c6
Mottais <mottais.afy@outlook.fr>
2023-11-22 02:00:25 -0800

    Gestion cas % et spécificateur décalés par un espace

--- _printf.c
+++ _printf.c
@@ -9,45 +9,44 @@
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int i, j, count = 0;
-
-	TypeDef_CodeFormat_Fonction CodeFormat_Fonction[] = {
+va_list args;
+int i, j, count = 0, decal;
+TypeDef_CodeFormat_Fonction CodeFormat_Fonction[] = {
 	{"c", print_char},
 	{"s", print_string},
-	{"d", print_decimal},
-	{"i", print_i_decimal},
-	{"%", print_unknown},
-	{NULL, NULL}
+	{"d", print_decimal}, {"i", print_i_decimal},
+	{"%", print_unknown}, {NULL, NULL}
 };
-	if (format == NULL)
-	return (-1);
 
-	va_start(args, format);
-	for (i = 0; format[i] != 0; i++)
+if (format == NULL)
+return (-1);
+va_start(args, format);
+for (i = 0; format[i] != 0; i++)
+{
+	if (format[i] == '%' && format[i + 1] != '\0')
 	{
-		if (format[i] == '%' && format[i + 1] != '\0')
+		decal = 0;
+		if (format[i + 1] == ' ')
+			decal = 1;
+		i = i + 1 + decal; /* Passer le caractère '%' et l'éventuel [espace] */
+		j = 0;
+		for (j = 0; CodeFormat_Fonction[j].Code_Format != 0; j++)
 		{
-			i++; /* Passer le caractère '%' */
-			j = 0;
-			for (j = 0; CodeFormat_Fonction[j].Code_Format != 0; j++)
+			if (format[i] == *CodeFormat_Fonction[j].Code_Format)
 			{
-				if (format[i] == *CodeFormat_Fonction[j].Code_Format)
-				{
-					count  = count + CodeFormat_Fonction[j].Pointeur_Fonction_Print(args);
-					break;
-				}
-			}
-			if (CodeFormat_Fonction[j].Code_Format == 0)
-			{
-				count = count + _putchar(format[i - 1]);
-				count = count + _putchar(format[i]);
+				count  = count + CodeFormat_Fonction[j].Pointeur_Fonction_Print(args);
+				break;
 			}
 		}
-	else
-	count = count + _putchar(format[i]);
+		if (CodeFormat_Fonction[j].Code_Format == 0)
+		{
+			count = count + _putchar(format[i + decal - 1]);
+			count = count + _putchar(format[i + decal]);
+		}
 	}
-	va_end(args);
-	return (count);
+	else
+		count = count + _putchar(format[i]);
 }
-
+va_end(args);
+return (count);
+}
